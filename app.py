1a958adfb6d5355d5d32a0cc6d1e4a6236a9b4f3
greed2854 <evan28542854@gmail.com>
2023-05-14 10:25:15 +0900

    app.py 변경됨

--- app.py
+++ app.py
@@ -1,35 +1,21 @@
-def classify_flower_color(flower_colors):
-    color_classes = {
-        '빨강': [],
-        '파랑': [],
-        '초록': [],
-        '노랑': [],
-        '흰색': []
-    }
+from flask import Flask
+app = Flask(__name__)
 
-    for color in flower_colors:
-        if '빨강' in color:
-            color_classes['빨강'].append(color)
-        elif '파랑' in color:
-            color_classes['파랑'].append(color)
-        elif '초록' in color:
-            color_classes['초록'].append(color)
-        elif '노랑' in color:
-            color_classes['노랑'].append(color)
-        elif '흰색' in color:
-            color_classes['흰색'].append(color)
-        else:
-            # 다른 색상에 해당하는 경우 여기에 처리 로직을 추가할 수 있습니다.
-            pass
+@app.route('/user/<username>')
+def show_user_profile(username):
+    # show the user profile for that user
+    return 'User %s' % username
 
-    return color_classes
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    # show the post with the given id, the id is an integer
+    return 'Post %d' % post_id
 
-# 테스트를 위한 리스트
-flowers = ['빨강장미', '파랑국화', '노랑튤립', '흰색카네이션', '초록장미', '보라꽃']
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
 
-# 꽃 색상으로 분류
-classified_colors = classify_flower_color(flowers)
-
-# 분류 결과 출력
-for color, flower_list in classified_colors.items():
-    print(color + ':', flower_list)
+if __name__ == '__main__':
+   
+    app.debug = True
+    app.run()
