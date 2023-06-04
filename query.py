4f3126d6f230f62dfd6887456a9b8eac6ca360c9
박석준 <parkseokjune@bagseogjun-ui-MacBookPro.local>
2023-06-04 11:18:36 +0900

    d

--- query.py
+++ query.py
@@ -13,7 +13,7 @@ CorrectAnswerRate = SolvedSum/ProblemNum
 Playtime = 'select playtime from user_info where user_id==%s'
 
 #총 획득 재화 (user_id, int)
-TotlaCash = 'select total_cash from user_info where user_id==%s'
+TotalCash = 'select total_cash from user_info where user_id==%s'
 
 #총 진행 일수 (user_id, int)
 DateSum = 'select date_sum from user_info where user_id==%s'
@@ -27,6 +27,7 @@ SendFriendSum = 'select count(*) from friends where user_id2==%s'
 #친구요청 받은 수 (user_id, int)
 RecieveFriendSum = 'Select count(*) from friends where user_id3==%s'
 
+
 #친구 수
 SendFriendSum+RecieveFriendSum
 
@@ -39,6 +40,51 @@ ProblemNumBySubject = 'select count(*) from solved where subject==%s and user_id
 #과목별 정답률
 CorrectAnswerRateBySubject = SolvedSumBySubject/ProblemNumBySubject
 
+#과목별 문제 불러오기 (subject, text)
+ProblemsBySubject = 'select * from problems where subject==%s'
+
+#cash 감소 (user_id, 변동할 양)int
+UpdateCash = 'update user_info set cash=cash+%d where user_id==%s'
+
+#totalcash 증가 (user_id, 증가할 값)int
+UpdateTotalCash = 'update user_info set total_cash=total_cash+%d where user_id==%s'
 
 
 
+
+
+
+
+
+
+
+'''
+과목 점수 유저아이디
+과학 80 2
+수학 100 2
+국어 80 1
+
+{
+    "과학":[
+        {
+            점수 : 80
+            유저아이디 : 2
+            푼 시간 : 30초
+        },
+        {
+            점수 : 70
+            유저아이디 : 3
+            푼 시간 : 50초
+        }
+    ],
+    "수학":[
+
+    ],
+    "국어":[
+
+    ]
+
+}
+
+
+'''
