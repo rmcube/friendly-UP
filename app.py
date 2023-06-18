9b0a9f89745b6afcffa59b357ad678fbf086aa39
greed2854 <evan28542854@gmail.com>
2023-06-18 11:22:22 +0900

    test2.py에 테스트 서버 구축 완료, DB연동 아직 안됨

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from query.user import getUsers
 
 app = Flask(__name__)
 
-@app.route('/users/<user_id>')
+@app.route('/users/<user_id>',method=["GET"])
 def show_user_profile(name):
     # show the user profile for that user
     return 'User %s' % name 
@@ -16,7 +16,6 @@ def get_users_by_condition():
     school = request.args.get('school')
     
     query = getUsers.query1
-    
     where = ""
     if(name!=None):
         where+="where name = %s"
@@ -47,14 +46,54 @@ def get_users_by_condition():
     return 'User %s' % name 
 
 
-@app.route('/users/<grade>')
-def show_user_profile(name):
-    # show the user profile for that user
-    return 'grade %s' % name 
+@app.route('/users/by/grade', method=["GET"])
+def get_user_by_condition(grade):
+    '''this router allow us to get a list of user info by grade and school.
+    '''
+    school = request.args.get('school')
+    grade = request.args.get('grade')
+   
+    query = getUsers.query1
+   
+    where = ""
+    if(grade!=None):
+        where+="where name = %s"
+    
+    if(school!=None):
+        if(where==""):
+            where+="where school = %s"
+        else:
+            where+= "and school = %s"
+    
+    cur.execute(query + where , grade, school)
+    
+    "select * from users where grade = %s and school = %s" , grade, school
+    return 'users %s' % grade   
 
+@app.route('/users/by/school', method=["GET"])
+def get_user_by_condition(school):
+    '''this router allow us to get a list of user info by school.
+    '''
+    school = request.args.get('school')
+   
+    query = getUsers.query1
+   
+    where = ""
+    
+    if(school!=None):
+        if(where==""):
+            where+="where school = %s"
+        else:
+            where+= "and school = %s"
+    
+    cur.execute(query + where , school)
+    
+    "select * from users where grade = %s and school = %s" , school
+    return 'users %s' % school   
 
 if __name__ == '__main__':
    
     app.debug = True
     app.run()
+    
     
---
+++ test2.py
@@ -0,0 +1,105 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+# 예시로 사용할 유저 데이터 리스트
+users = [
+    {
+        'user_id': 1,
+        'name': 'John',
+        'school': 'ABC High School',
+        'grade': 10,
+        'createdAt': '2022-01-01',
+        'updatedAt': '2022-01-01'
+    },
+    {
+        'user_id': 2,
+        'name': 'Alice',
+        'school': 'XYZ Middle School',
+        'grade': 9,
+        'createdAt': '2022-02-01',
+        'updatedAt': '2022-02-01'
+    },
+    {
+        'user_id': 3,
+        'name': 'Bob',
+        'school': 'ABC High School',
+        'grade': 11,
+        'createdAt': '2022-03-01',
+        'updatedAt': '2022-03-01'
+    }
+]
+
+# 1. GET /user/{user_id}
+@app.route('/users/<int:user_id>', methods=['GET'])
+def get_user(user_id):
+    print(user_id)
+    for user in users:
+        if user['user_id'] == user_id:
+            return jsonify(user), 200
+    return jsonify({'error': 'User not found'}), 404
+
+# 2. GET /users?name={name}&grade={grade}&school={school}
+@app.route('/users', methods=['GET'])
+def get_users():
+    name = request.args.get('name')
+    grade = request.args.get('grade')
+    school = request.args.get('school')
+
+    filtered_users = []
+    for user in users:
+        if name in user['name'] and school in user['school'] and user['grade'] >= int(grade):
+            filtered_users.append(user)
+
+    return jsonify(filtered_users), 200
+
+# 3. POST /user
+@app.route('/user', methods=['POST'])
+def create_user():
+    data = request.get_json()
+    if 'name' not in data or 'school' not in data or 'grade' not in data:
+        return jsonify({'error': 'Missing fields'}), 400
+
+    user = {
+        'user_id': len(users) + 1,
+        'name': data['name'],
+        'school': data['school'],
+        'grade': data['grade'],
+        'createdAt': '2023-05-12',
+        'updatedAt': '2023-05-12'
+    }
+
+    users.append(user)
+    return jsonify(user), 201
+
+# 4. DELETE /user/{user_id}
+@app.route('/user/<int:user_id>', methods=['DELETE'])
+def delete_user(user_id):
+    for user in users:
+        if user['user_id'] == user_id:
+            users.remove(user)
+            return jsonify({'message': 'User deleted'}), 200
+    return jsonify({'error': 'User not found'}), 404
+
+# 5. PUT /user/{user_id}
+@app.route('/user/<int:user_id>', methods=['PUT'])
+def update_user(user_id):
+    data = request.get_json()
+    for user in users:
+        if user['user_id'] == user_id:
+            if 'name' in data:
+                user['name'] = data['name']
+            if 'school' in data:
+                user['school'] = data['school']
+            if 'grade' in data:
+                user
+            if 'grade' in data:
+                user['grade'] = data['grade']
+            
+            user['updatedAt'] = '2023-05-12'  # 업데이트된 날짜로 설정
+            return jsonify(user), 200
+    
+    return jsonify({'error': 'User not found'}), 404
+
+if __name__ == '__main__':
+    app.run()
