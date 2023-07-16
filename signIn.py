2c40c83e5775543ac3729c92aa0ed3a659c13d98
박석준 <parkseokjune@bagseogjun-ui-MacBookPro.local>
2023-07-16 11:22:58 +0900

    d

--- signIn.py
+++ signIn.py
@@ -12,8 +12,8 @@ sql1="insert into user (name, grade, school, created_at, updated_at) values(%s, 
 sql2="insert into user_info(password, cash, prefer_subject, playtime, total_cash, date_sum, problem_sum, problem_solved, created_at, updated_at, send_sum, share_sum) values(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
 
 print("asdf")
-passwordList=cur.execute("select password from user_info")
-print(passwordList)
+cur.execute("select password from user_info")
+
 
 
 def sign_in(name, grade, school, password, prefer_subject):
