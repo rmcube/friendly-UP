00b60f5d56124286584d82d9eff05ceabb0a4933
rmcube <75253135+rmcube@users.noreply.github.com>
2023-07-17 18:09:52 +0900

    Add files via upload
    flask

--- flask.py
+++ flask.py
@@ -1,6 +1,6 @@
 import requests
 
-url = 'http://localhost:5000/test' # POST 요청을 보낼 URL
+url = 'http://localhost:5000/' # POST 요청을 보낼 URL
 data = {'key1': 'value1', 'key2': 'value2'} # 전송할 데이터
 response = requests.post(url, json=data)
 
