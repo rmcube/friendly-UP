610aebd51cdf2f547ed4492d32b4bbdd182d1c75
rmcube <75253135+rmcube@users.noreply.github.com>
2023-07-22 16:38:30 +0900

    ㅁㄴasas
    ㅁㄴasas

--- app.py
+++ app.py
@@ -1,102 +1,98 @@
-from flask import Flask,request
+from flask import Flask, request
 from query.user import getUsers
 
 app = Flask(__name__)
 
-@app.route('/users/<user_id>',method=["GET"])
-def show_user_profile(name):
+
+@app.route("/users/<user_id>", methods=["GET"])
+def show_user_profile(user_id):
     # show the user profile for that user
-    return 'User %s' % name 
+    return "User %s" % user_id
 
-@app.route('/users')
+
+@app.route("/users")
 def get_users_by_condition():
-    '''this router allow us to get a list of user info by name and school.
-    '''
-    school = request.args.get('school')
-    
+    name = request.args.get("name")
+    school = request.args.get("school")
+
     query = getUsers.query1
     where = ""
-    if(name!=None):
-        where+="where name = %s"
-    
-    if(school!=None):
-        if(where==""):
-            where+="where school = %s"
+    if name is not None:
+        where += "where name = %s"
+
+    if school is not None:
+        if where == "":
+            where += "where school = %s"
         else:
-            where+= "and school = %s"
-    
-    cur.execute(query + where , name, school)
-    
-    "select * from users where name = %s and school = %s" , name, school
-    
-    [
-        {
-            "user_id" : 1231231,
-            "name":"solmin",
-            "school":"yeonsong high school"
-        },
-        {
-            "user_id" : 13123,
-            "name":"so123123lmin",
-            "school":"yeonsong123123 high school"
-        }
-    ]
-# show the user profile for that user
-    return 'User %s' % name 
+            where += " and school = %s"
+
+    # You need to establish a connection to the database and create a cursor here
+    # For example:
+    # connection = your_database_connection
+    # cur = connection.cursor()
+
+    cur.execute(query + where, (name, school))
+
+    # Add logic here to fetch the result and convert it to a dictionary
+
+    return "User %s" % name
 
 
-<<<<<<< HEAD
-@app.route('/users/by/grade', method=["GET"])
-def get_user_by_condition(grade):
-    '''this router allow us to get a list of user info by grade and school.
-    '''
-    school = request.args.get('school')
-    grade = request.args.get('grade')
-   
+@app.route("/users/by/grade", methods=["GET"])
+def get_user_by_grade():
+    school = request.args.get("school")
+    grade = request.args.get("grade")
+
     query = getUsers.query1
-   
-    where = ""
-    if(grade!=None):
-        where+="where name = %s"
-    
-    if(school!=None):
-        if(where==""):
-            where+="where school = %s"
-        else:
-            where+= "and school = %s"
-    
-    cur.execute(query + where , grade, school)
-    
-    "select * from users where grade = %s and school = %s" , grade, school
-    return 'users %s' % grade   
 
-@app.route('/users/by/school', method=["GET"])
-def get_user_by_condition(school):
-    '''this router allow us to get a list of user info by school.
-    '''
-    school = request.args.get('school')
-   
+    where = ""
+    if grade is not None:
+        where += "where grade = %s"
+
+    if school is not None:
+        if where == "":
+            where += "where school = %s"
+        else:
+            where += " and school = %s"
+
+    # You need to establish a connection to the database and create a cursor here
+    # For example:
+    # connection = your_database_connection
+    # cur = connection.cursor()
+
+    cur.execute(query + where, (grade, school))
+
+    # Add logic here to fetch the result and convert it to a dictionary
+
+    return "users %s" % grade
+
+
+@app.route("/users/by/school", methods=["GET"])
+def get_user_by_school():
+    school = request.args.get("school")
+
     query = getUsers.query1
-   
-    where = ""
-    
-    if(school!=None):
-        if(where==""):
-            where+="where school = %s"
-        else:
-            where+= "and school = %s"
-    
-    cur.execute(query + where , school)
-    
-    "select * from users where grade = %s and school = %s" , school
-    return 'users %s' % school   
 
-=======
->>>>>>> 96d0ff3fc03e69a9c967e16187b90a2ae7449c67
-if __name__ == '__main__':
-   
+    where = ""
+
+    if school is not None:
+        if where == "":
+            where += "where school = %s"
+        else:
+            where += " and school = %s"
+
+    # You need to establish a connection to the database and create a cursor here
+    # For example:
+    # connection = your_database_connection
+    # cur = connection.cursor()
+
+    cur.execute(query + where, (school,))
+
+    # Add logic here to fetch the result and convert it to a dictionary
+
+    return "users %s" % school
+
+
+if __name__ == "__main__":
     app.debug = True
-    app.run(host='127.0.0.1', port=5000)
-
-    
-    
+    app.run(host="127.0.0.1", port=5000)
