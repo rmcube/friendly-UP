e93f393be8cd6ce775c8fda44f4798e4730191f8
rmcube <75253135+rmcube@users.noreply.github.com>
2023-07-23 11:15:42 +0900

    12112
    21ㅁㅇㅁㅇ

--- app.py
+++ app.py
@@ -59,10 +59,10 @@ def get_user(user_id):
         user = cur.fetchone()
 
         if user is None:
-            return jsonify({"message": "해당 user_id에 해당하는 유저 정보가 없습니다."}), 404
+            return jsonify({"message": "해당 user_id에 해당하는 유저 정보가 없습니다."}), 1004
 
         # 조회한 유저 정보 반환
-        return jsonify(user), 200
+        return jsonify(user_id), 200
     except Exception as e:
         return jsonify({"message": str(e)}), 500
 
