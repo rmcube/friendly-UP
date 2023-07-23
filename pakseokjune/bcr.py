4ddcdf47a3b2c54268da076eed777f88388c6c7e
greed <106300048+greed2854@users.noreply.github.com>
2023-07-23 10:43:18 +0900

    gitignore 재적용

--- pakseokjune/bcr.py
+++
@@ -1,6 +0,0 @@
-import bcrypt
-password=b'asdf'
-salt = bcrypt.gensalt()
-hashed = bcrypt.hashpw(password, salt)
-print(salt)
-print(hashed)
--- pakseokjune/flaskapp/venv/start.py
+++
@@ -1,6 +0,0 @@
-from flask import Flask, request, jsonify
-import pymysql
-
-app = Flask(__name__)
-if __name__ == '__main__':
-    app.run()
