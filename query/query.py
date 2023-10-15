b4e3030c3b15d40d144453034fab719f557465ab
박석준 <parkseokjune@bagseogjun-ui-MacBookPro.local>
2023-10-16 00:30:35 +0900

    query

--- query/query.py
+++ query/query.py
@@ -2,16 +2,16 @@
 CorrectAnswerRate = "select total.b/total.a from (select count(*) as a, sum(is_correct) as b from solved where user_id = %s) as total"
 
 # 총 소요 시간 (user_id, int)
-Playtime = "select playtime from user_info where user_id=%s"
+Playtime = "select playtime from user where user_id=%s"
 
 # 총 획득 재화 (user_id, int)
-TotalCash = "select total_cash from user_info where user_id=%s"
+TotalCash = "select total_cash from user where user_id=%s"
 
 # 총 진행 일수 (user_id, int)
-DateSum = "select date_sum from user_info where user_id=%s"
+DateSum = "select date_sum from user where user_id=%s"
 
 # 친구에게 문제 보내기 횟수 (user_id, int)
-ShareSum = "select share_sum from user_info where user_id=%s"
+ShareSum = "select share_sum from user where user_id=%s"
 
 # 친구요청 보낸 수 (user_id, int)
 SendFriendSum = "select count(*) from friends where user_id2=%s"
@@ -36,15 +36,15 @@ ProblemsBySubject = "select * from problems where subject=%s"
 
 # cash변동 (user_id, 변동할 양)int
 a = 10
-cash = "select cash from user_info where user=%d"
+cash = "select cash from user where user=%d"
 
-UpdateCash = "update user_info set cash=%d where user_id=%d"
+UpdateCash = "update user set cash=%d where user_id=%d"
 
 # totalcash 증가 (user_id, 증가할 값)int
-UpdateTotalCash = "update user_info set total_cash=total_cash+%d where user_id=%s"
+UpdateTotalCash = "update user set total_cash=total_cash+%d where user_id=%s"
 
 # datesum 증가(+1일) 전체에게
-PlusDateSum = "update user_info set date_sum=date_sum+1"
+PlusDateSum = "update user set date_sum=date_sum+1"
 
 # alram 삭제(user, int)
 DeleteAlarm = "delete from alarm where user_id=%s"
--- query/user_info.py
+++ query/user_info.py
@@ -1,20 +1,20 @@
 #총 소요 시간 (user_id, int)
-Playtime = 'select playtime from user_info where user_id=%s'
+Playtime = 'select playtime from user where user_id=%s'
 
 #총 획득 재화 (user_id, int)
-TotalCash = 'select total_cash from user_info where user_id=%s'
+TotalCash = 'select total_cash from user where user_id=%s'
 
 #총 진행 일수 (user_id, int)
-DateSum = 'select date_sum from user_info where user_id=%s'
+DateSum = 'select date_sum from user where user_id=%s'
 
 #친구에게 문제 보내기 횟수 (user_id, int) 
-ShareSum = 'select share_sum from user_info where user_id=%s'
+ShareSum = 'select share_sum from user where user_id=%s'
 
 #cash 감소 (user_id, 변동할 양)int
-UpdateCash = 'update user_info set cash=cash+%d where user_id=%s'
+UpdateCash = 'update user set cash=cash+%d where user_id=%s'
 
 #totalcash 증가 (user_id, 증가할 값)int
-UpdateTotalCash = 'update user_info set total_cash=total_cash+%d where user_id=%s'
+UpdateTotalCash = 'update user set total_cash=total_cash+%d where user_id=%s'
 
 #datesum 증가(+1일) 전체에게
-PlusDateSum = 'update user_info set date_sum=date_sum+1'
+PlusDateSum = 'update user set date_sum=date_sum+1'
