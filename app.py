c4427459bf66ef908c5fbbdf606de335b350c8b5
rmcube <75253135+rmcube@users.noreply.github.com>
2023-10-22 13:19:58 +0900

    ㅁㅇㅁㅇㅁㅇ

--- app.py
+++ app.py
@@ -165,5 +165,36 @@ def get_user(user_id):
         conn.close()
 
 
+# 특정 유저의 상세 정보 조회
+@app.route("/api/user/detail", methods=["POST"])
+def get_user2(user_id):
+    try:
+        # 데이터베이스 연결 및 쿼리 실행
+        conn = get_db_connection()
+        data = request.get_json()
+        user_id = data.get("user_id")
+        value = data.get("value")
+        query_string = "SELECT %s FROM user WHERE user_id = %s"
+        with conn.cursor() as cur:
+            cur.execute(
+                query_string,
+                (user_id),
+                (value),
+            )
+            user = cur.fetchone()
+
+            if user is None:
+                return jsonify({"message": "해당 user_id에 해당하는 유저 정보가 없습니다."}), 404
+
+            # 조회한 유저 정보 반환
+            return jsonify(user), 200
+
+    except Exception as e:
+        return jsonify({"message": str(e)}), 500
+
+    finally:
+        conn.close()
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
